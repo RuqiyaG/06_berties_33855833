e91a6e03da133f948d16f724fe77e257667a7213
RuqiyaG <ruqiyageelle@gmail.com>
2025-11-14 00:04:37 +0000

    made add book confirmation route

--- routes/books.js
+++ routes/books.js
@@ -1,4 +1,5 @@
 // Create a new router
+const { name } = require("ejs");
 const express = require("express")
 const router = express.Router()
 
@@ -22,8 +23,24 @@ router.get('/search-result', function (req, res, next) {
     res.send("You searched for: " + req.query.keyword)
 });
 
+//new route for adding books
 router.get('/addbook', function(req, res, next) {
     res.render("addbook.ejs");
+});
+
+// will redirect you another page to confirm you have added the book
+router.post('/bookadded', function(req, res, next) {
+    // query to insert a new book into database
+    let sqlquery = "INSERT INTO books (name) VALUES(?)";
+    let newrecord = [req.body.name];
+
+    db.query(sqlquery, newrecord, (err, result) => {
+        if(err) {
+            next(err)
+        }
+        res.send('This book has been added:' + req.body.name)
+      });
+
 });
 
 // Export the router object so index.js can access it
