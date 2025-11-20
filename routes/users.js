a00613d631d2966f116c3129941d9c979863a170
RuqiyaG <ruqiyageelle@gmail.com>
2025-11-20 21:52:24 +0000

    added hash password

--- routes/users.js
+++ routes/users.js
@@ -13,11 +13,14 @@ router.post('/registered', function (req, res, next) {
     const plainPassword = req.body.password
     // stores hashed password in database
     bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword){
+        let result = ' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email
+        result += 'Your password is:'+ req.body.password + 'and your hashed password is:'+ hashedPassword
 
+        res.send(result) 
     });
 
     // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+    //res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                            
 }); 
 
 // Export the router object so index.js can access it
