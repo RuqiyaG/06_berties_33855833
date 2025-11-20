9bdeab461b8547380df1b4748f4882003de537bc
RuqiyaG <ruqiyageelle@gmail.com>
2025-11-20 21:04:30 +0000

    installed bcrypt and set it up in my code and updated the readme

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,21 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
 })
 
 router.post('/registered', function (req, res, next) {
+    //
+    const plainPassword = req.body.password
+    // stores hashed password in database
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword){
+
+    });
+
     // saving data in database
     res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
 }); 
