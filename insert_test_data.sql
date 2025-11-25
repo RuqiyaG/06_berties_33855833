18d55ee1edc33fe5dc7ca876db1ca03c7f084edf
RuqiyaG <ruqiyageelle@gmail.com>
2025-11-25 15:27:36 +0000

    added user gold for marking

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,5 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99), ('Wheel of Time', 15.99), ('Homo Sapiens', 13.99) ;
+
+INSERT INTO users (name, password)VALUES('gold', 'smiths')
