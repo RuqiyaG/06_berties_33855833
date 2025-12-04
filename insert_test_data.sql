61c9a81f52e93e5cc18db4b5ee662d41960058bf
RuqiyaG <ruqiyageelle@gmail.com>
2025-12-04 23:12:52 +0000

    added extra books under £10

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,5 +2,5 @@
 
 USE berties_books;
 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99), ('Wheel of Time', 15.99), ('Homo Sapiens', 13.99) ;
+INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99), ('Wheel of Time', 15.99), ('Homo Sapiens', 13.99), ('How To Cook', 7.99), ('The Way The Universe Works', 9.99) ;
 
